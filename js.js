64db564e6096709e21c87f39f7830c0b2a32dbce
vikisopi-qa <sopivik@gmail.com>
2025-11-06 16:29:58 +0400

    Add files via upload

---
+++ js.js
@@ -0,0 +1,16 @@
+/* Это объявление переменной, мы наши кнопку по тегу */
+const button = document.querySelector('button');
+
+/* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
+button.addEventListener('click', function() {
+	alert('верно! 2 кг')
+})
+
+
+
+Kibana 
+https://kibana.pokemonbattle.ru
+
+• Логин: student@qa.studio
+• Пароль: 3hj%KQghy67frw
+
